e7af97823cf6582550d2515f773670b4aa4885b7
EQSDX <128876125+EQSDX@users.noreply.github.com>
2023-07-16 17:23:17 +0700

    update

---
+++ EQ/src/com/AI/Sa.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row">
+        <div class="col-6">
+            <img src="/images/dog.jpeg" width="250" alt="">
+        </div>
+        <div class="col-6">
+            <div>
+                
+            </div>
+            <div>
+                aaaaaaaaaaaaaaabbbbbbbbbbbbbbbb
+            </div>
+            <div class="text-end">
+                <h1>Mr.Dogger</h1>
+            </div>
+        </div>
+    </div>
+</template>
