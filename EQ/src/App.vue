8f085651e5cf278fd2bd2b05c672ba57bacd4772
EQSDX <128876125+EQSDX@users.noreply.github.com>
2023-07-16 17:01:17 +0700

    Revert "EQ-version1.1"
    This reverts commit fe995b05abe52dd6fbd4ee62714f65a5e214dd9c.

--- EQ/src/App.vue
+++ EQ/src/App.vue
@@ -1,14 +1,12 @@
 <script setup>
-import Im from './com/Im.vue';
 </script>
 
 <template>
   <div class="d-flex flex-row mb-3">
-    <div class="p-2"></div>
-    <div lang="la" class="p-2"></div>
+    <div class="p-2">ສະບາຍດີ</div>
+    <div lang="la" class="p-2">ສະບາຍດີ wold/div>
     <div class="p-2">
-      <Im/>
-      <!-- <span class="material-symbols-outlined"> search </span> -->
+      <span class="material-symbols-outlined"> search </span>
     </div>
   </div>
 </template>
